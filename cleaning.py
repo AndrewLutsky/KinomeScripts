c56153ce0868e9836996efc73cab26014ef5bfd0
katztyler01 <98284658+katztyler01@users.noreply.github.com>
2024-02-05 14:50:22 -0500

    updating cleaning.py
    also includes merged all hetero atoms pdb

--- cleaning.py
+++ cleaning.py
@@ -74,6 +74,22 @@ def extract_hetatms_with_phosphorous(pdb_files_dir, resname_to_formula_dict, out
         for hetatm_line in hetatms_with_p:
             output_file.write(hetatm_line)
 
+def extract_all_hetatms(pdb_files_dir, output_pdb_path):
+    hetatms = []  # List to hold HETATM lines for output
+
+    for filename in os.listdir(pdb_files_dir):
+        if filename.endswith(".pdb"):
+            file_path = os.path.join(pdb_files_dir, filename)
+            with open(file_path, 'r') as pdb_file:
+                for line in pdb_file:
+                    if line.startswith("HETATM"):
+                        hetatms.append(line)
+
+    # Write the collected HETATM lines to the output PDB file
+    with open(output_pdb_path, 'w') as output_file:
+        for hetatm_line in hetatms:
+            output_file.write(hetatm_line)
+
 def isolate_proteins(input_dir, output_dir):
     # Create the output directory if it does not exist
     os.makedirs(output_dir, exist_ok=True)
@@ -89,12 +105,24 @@ def isolate_proteins(input_dir, output_dir):
                     if not line.startswith("HETATM"):
                         output_file.write(line)
 
-
+'''
+## Run this to isolate protiens
 input_dir = 'pdb_aligned'
 output_dir = 'pdb_aligned_protein'
 isolate_proteins(input_dir, output_dir)
+'''
+
+## Run this to extract all hetero atoms to one merged pdb
+pdb_files_dir = 'pdb_aligned' 
+output_pdb_path = 'hetatms_all.pdb'  
+extract_all_hetatms(pdb_files_dir, output_pdb_path)
+
+
+
+
 
 '''
+## Run this to output pdb with all hetatms containing phosphorus
 cif_path = 'components.cif'
 pdb_files_dir = 'pdb_aligned'  # Directory containing PDB files
 output_pdb_path = 'hetatms_with_p.pdb'  # Path for the output PDB file
