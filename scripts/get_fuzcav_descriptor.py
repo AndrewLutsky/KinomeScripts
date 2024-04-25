10834421b152964f34ce8c9dce308c55b1c7ca90
katztyler01 <98284658+katztyler01@users.noreply.github.com>
2024-04-24 23:14:27 -0400

    add roc_plots.py

--- scripts/get_fuzcav_descriptor.py
+++
@@ -1,7 +0,0 @@
-with open("fuzcav_dist.txt","r") as f:
-    lines = f.readlines()
-    dict = {}
-    for line in lines:
-        line = line.split()
-        dict[(line[0],line[1])] = float(line[2])
-
